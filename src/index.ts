5065128307d3a888f5bc62f4026766a22064426d
Alexander Nordlund <beelzenord@gmail.com>
2025-07-03 09:48:13 +0200

    reverted to raw primitives

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { NumberNodeInstance,BinaryOperation } from "./dst/dst";
 
+const node = new BinaryOperation(new NumberNodeInstance(1), new NumberNodeInstance(3),"+");
 
+console.log(JSON.stringify(node));
